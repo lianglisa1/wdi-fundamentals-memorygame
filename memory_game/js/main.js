f023ff582b06b2fd67c387aa5b4c7d051aaaa79b
Lisa <lianglisa1@gmail.com>
2020-03-05 18:32:37 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
