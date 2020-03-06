2d77701efe6aecbd329518c97735285956f2dbe6
Lisa <lianglisa1@gmail.com>
2020-03-05 21:41:34 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,18 +3,24 @@ console.log("Up and running!");
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.")
-	}
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardInPlay[0] === cardInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+	} 
 }
+
+function flipCard(cardId) {
+	checkForMatch();
+	console.log ("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+} 
+
+flipCard(0);
+flipCard(2);
+
+//suppose to display "Sorry, try again." in console but is not showing. Review Project Functions again
